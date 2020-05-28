7dfa1e6da09a411007b544317302c5b309e0be6c
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-05-28 20:49:41 -0300

    Controller Layer - WelcomeController.java

---
+++ src/main/java/com/neutronstar/todoapp/controllers/WellcomeController.java
@@ -0,0 +1,33 @@
+package com.neutronstar.todoapp.controllers;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WellcomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String showWellcomePage(ModelMap model) {
+		model.put("name", getLoggedinUserName());
+		return "welcome";
+	}
+	
+	private String getLoggedinUserName() {
+		Object principal = SecurityContextHolder.getContext()
+		.getAuthentication().getPrincipal();
+		
+		if(principal instanceof UserDetails) {
+		     return ((UserDetails) principal).getUsername();
+        }
+
+        return principal.toString();
+    }
+		
+	}
+	
+
+
