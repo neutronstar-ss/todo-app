fa8c00ca89a792f6295551cba6f85ee149f3728a
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-05-28 21:10:36 -0300

    Service Layer - ITodoService.java

--- src/main/java/com/neutronstar/todoapp/services/ITodoService.java
+++ src/main/java/com/neutronstar/todoapp/services/ITodoService.java
@@ -1,11 +1,23 @@
 package com.neutronstar.todoapp.services;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import com.neutronstar.todoapp.models.Todo;
 
 public interface ITodoService {
 	
 	List<Todo> getTodosByUser(String user);
+	
+	Optional<Todo> getTodoById(Long id);
+	
+	void updateTodo(Todo todo);
+	
+	void addTodo(String name, String desc, Date targetDate, boolean isDone);
+	
+	void deleteTodo(Long id);
+	
+	void saveTodo(Todo todo);
 
 }
